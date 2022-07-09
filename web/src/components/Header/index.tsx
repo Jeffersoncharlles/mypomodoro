9a1d7c74e8a19db04e8222cced88c58767da0d6b
Jefferson Charlles <contato@jefferdeveloper.com>
2022-07-09 10:37:39 -0300

    chore:layout header ok

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,5 +1,23 @@
-import { Container } from './styles'
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+import { Logo } from '../Logo'
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
-  return <Container></Container>
+  return (
+    <HeaderContainer>
+      <span>
+        <Logo />
+      </span>
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
--- web/src/components/Header/styles.ts
+++ web/src/components/Header/styles.ts
@@ -1,3 +1,34 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      color: ${({ theme }) => theme['gray-100']};
+
+      border-top: 3px solid transparent; //coloco para ficar centralizado
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${({ theme }) => theme['green-500']};
+      }
+
+      //quando usa o NavLink ele da essa class quando clica
+      &.active {
+        color: ${({ theme }) => theme['green-500']};
+      }
+    }
+  }
+`
